e1bc72b0edc8d1e9b28be643f6a580fa4927a29b
Hekkura <54793875+Hekkura@users.noreply.github.com>
2022-03-01 22:36:19 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+var slideIndex = 1;
+
+displaySlides(slideIndex);
+
+function pressButton(n){
+    displaySlides(slideIndex+=n);
+}
+
+function displaySlides(n){
+    var i;
+    var slides = document.getElementsByClassName('banner');
+
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+    }
+    for(i=0; i<slides.length;i++){
+        slides[i].className = slides[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.opacity = 1;
+    slides[slideIndex-1].className += " active";
+
+    
+}
+
